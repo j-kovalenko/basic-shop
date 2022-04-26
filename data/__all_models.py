e543c1275a3b6928cb1b96f348ae4470503b1e2d
j-kovalenko <stdnkp@gmail.com>
2022-04-26 10:36:33 +0300

    cart without +/-. finished add acc

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,2 +1,3 @@
 from . import hoodies
 from . import tshirts
+from . import accessories
--- main.py
+++ main.py
@@ -4,35 +4,39 @@ import data.tshirts
 from data import db_session
 from data.hoodies import Hoodie
 from data.tshirts import Tshirt
+# from data.accessories import import Acc
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 db_session.global_init("db/shop.db")
-# db_sess = db_session.create_session()
-# for item in db_sess.query(Tshirt).filter(Tshirt.id == 1):
-#     print(item.__dict__)
-# hoodies = [item.unique_id for item in db_sess.query(Hoodie).all()]
-# tshirts = [item.unique_id for item in db_sess.query(Tshirt).all()]
-# db_sess = db_session.create_session()
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     page_name = "Домашняя страница"
+    # session.pop('cart', None)
+    # session.pop('total', None)
     return render_template('index.html', name=page_name)
 
 
 @app.route('/cart')
 def cart():
     page_name = "Корзина"
-    cart = list(session.get('cart', {}).keys())
-    total = session.get('total', 0)
     db_session.global_init("db/shop.db")
     db_sess = db_session.create_session()
-    return render_template('cart.html', name=page_name, cart=cart, total=total, hoodies=hoodies, tshirts=tshirts,
-                           db_sess=db_sess, Hoodie=Hoodie, Tshirt=Tshirt)
+    cart = session.get('cart', [])
+    user_cart = []
+    for it in cart:
+        if it[1] == 'hoodies':
+            item = db_sess.query(Hoodie).filter(Hoodie.name_id == it[0]).first()
+        elif it[1] == 'tshirts':
+            item = db_sess.query(Tshirt).filter(Tshirt.name_id == it[0]).first()
+        user_cart.append(item)
+    total = session.get('total', 0)
+    # print(user_cart)
+    return render_template('cart.html', name=page_name, cart=user_cart, total=total)
 
 
 @app.route('/hoodies')
@@ -46,6 +50,7 @@ def hoodies():
 @app.route('/<type>/<good>', methods=['POST', 'GET'])
 def goods_page(type, good):
     db_session.global_init("db/shop.db")
+    cart = session.get('cart', [])
     try:
         db_sess = db_session.create_session()
         if type == 'hoodies':
@@ -58,25 +63,31 @@ def goods_page(type, good):
         return 'такого наименования нет'
     if request.method == 'GET':
         past = f"../{type}"
-        return render_template('product.html', item=item, name=page_name, past=past)
+        return render_template('product.html', item=item, name=page_name, past=past, cart=cart)
     elif request.method == 'POST':
-        if request.form['add'] == 'add':
-            # db_sess = db_session.create_session()
-            # if type == 'hoodies':
-            #     item = db_sess.query(Hoodie).filter(Hoodie.name_id == good).first()
-            # elif type == 'tshirts':
-            #     item = db_sess.query(Tshirt).filter(Tshirt.name_id == good).first()
-            # else:
-            #     return 'невозможно добавить товар в корзину - товар не найден'
+        # try:
+        if request.form['action'] == 'add':
             cart = session.get('cart', [])
-            cart += [item]
+            cart += [[item.name_id, item.__table__.name]]
             session['cart'] = cart
             total = session.get('total', 0)
             total += item.price
             session['total'] = total
             # print(cart)
             print(session)
-            return redirect(f'/{type}/{good}')
+        if request.form['action'] == 'delete':
+            cart = session.get('cart', [])
+            if [item.name_id, item.__table__.name] in cart:
+                cart.remove([item.name_id, item.__table__.name])
+            else:
+                return f"Товар {item.name} уже не в корзине"
+            session['cart'] = cart
+            total = session.get('total', 0)
+            total -= item.price
+            session['total'] = total
+        # except Exception as e:
+        #     print(e)
+        return redirect(f'/{type}/{good}')
 
 
 @app.route('/test', methods=['POST', 'GET'])
@@ -93,7 +104,7 @@ def test():
         cart['testkey'] = 'testval'
         print(cart)
 
-        # session.pop('cart', None)
+        session.pop('cart', None)
         # session.pop('testcart', None)
         return render_template('test.html')
     db_session.global_init("db/shop.db")
