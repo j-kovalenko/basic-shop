e2d51c3157156b65e86ba1bb63c02ca246620d89
j-kovalenko <stdnkp@gmail.com>
2022-04-28 14:08:14 +0300

    add delete item from /cart

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import data.tshirts
 from data import db_session
 from data.hoodies import Hoodie
 from data.tshirts import Tshirt
-# from data.accessories import import Acc
+from data.accessories import Acc
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -21,24 +21,45 @@ def index():
     return render_template('index.html', name=page_name)
 
 
-@app.route('/cart')
+@app.route('/cart', methods=['POST', 'GET'])
 def cart():
-    page_name = "Корзина"
-    db_session.global_init("db/shop.db")
-    db_sess = db_session.create_session()
-    cart = session.get('cart', [])
-    user_cart = []
-    for it in cart:
-        if it[1] == 'hoodies':
-            item = db_sess.query(Hoodie).filter(Hoodie.name_id == it[0]).first()
-        elif it[1] == 'tshirts':
-            item = db_sess.query(Tshirt).filter(Tshirt.name_id == it[0]).first()
-        user_cart.append(item)
-    total = session.get('total', 0)
-    # print(user_cart)
-    return render_template('cart.html', name=page_name, cart=user_cart, total=total)
+    if request.method == 'GET':
+        page_name = "Корзина"
+        db_session.global_init("db/shop.db")
+        db_sess = db_session.create_session()
+        cart = session.get('cart', [])
+        user_cart = []
+        for it in cart:
+            if it[1] == 'hoodies':
+                item = db_sess.query(Hoodie).filter(Hoodie.name_id == it[0]).first()
+            elif it[1] == 'tshirts':
+                item = db_sess.query(Tshirt).filter(Tshirt.name_id == it[0]).first()
+            elif it[1] == 'accessories':
+                item = db_sess.query(Acc).filter(Acc.name_id == it[0]).first()
+            user_cart.append(item)
+        total = session.get('total', 0)
+        # print(user_cart)
+        return render_template('cart.html', name=page_name, cart=user_cart, total=total)
+    elif request.method == 'POST':
+        if request.form['delete']:
+            resp = request.form['delete'].split('-')
+            cart_item = [resp[0], resp[1]]
+            cart = session.get('cart', [])
+            if cart_item in cart:
+                cart.remove(cart_item)
+            else:
+                return f"Товар {cart_item[0]} уже не в корзине"
+            session['cart'] = cart
+            total = session.get('total', 0)
+            total -= int(resp[2])
+            session['total'] = total
+        # except Exception as e:
+        #     print(e)
+        return redirect(f'/cart')
 
 
+
+@app.route('/hoodies/')
 @app.route('/hoodies')
 def hoodies():
     page_name = "Каталог кофт"
@@ -58,6 +79,9 @@ def goods_page(type, good):
             page_name = item.name
         elif type == 'tshirts':
             item = db_sess.query(Tshirt).filter(Tshirt.name_id == good).first()
+            page_name = item.name
+        elif type == 'accessories':
+            item = db_sess.query(Acc).filter(Acc.name_id == good).first()
             page_name = item.name
     except AttributeError:
         return 'такого наименования нет'
@@ -90,44 +114,22 @@ def goods_page(type, good):
         return redirect(f'/{type}/{good}')
 
 
-@app.route('/test', methods=['POST', 'GET'])
-def test():
-    if request.method == 'GET':
-        return render_template('test.html')
-    elif request.method == 'POST':
-        print('clicked')
-        print(request.form['index'])
-        testcart = session.get('testcart', [])
-        session['testcart'] = testcart + ['d']
-        print(session['testcart'])
-        cart = session.get('cart', {})
-        cart['testkey'] = 'testval'
-        print(cart)
-
-        session.pop('cart', None)
-        # session.pop('testcart', None)
-        return render_template('test.html')
-    db_session.global_init("db/shop.db")
-    # try:
-    #     db_sess = db_session.create_session()
-    #     if type == 'hoodies':
-    #         item = db_sess.query(Hoodie).filter(Hoodie.name_id == good).first()
-    #         page_name = item.name
-    #     elif type == 'tshirts':
-    #         item = db_sess.query(Tshirt).filter(Tshirt.name_id == good).first()
-    #         page_name = item.name
-    # except AttributeError:
-    #     return 'такого наименования нет'
-    # past = f"../{type}"
-    # return render_template('product.html', item=item, name=page_name, past=past)
-
-
+@app.route('/tshirts/')
 @app.route('/tshirts')
 def tshirts():
     page_name = "Каталог футболок"
     db_session.global_init("db/shop.db")
     db_sess = db_session.create_session()
     return render_template('section.html', name=page_name, db_sess=db_sess, sect=Tshirt)
+
+
+@app.route('/accessories/')
+@app.route('/accessories')
+def accessories():
+    page_name = "Каталог аксессуаров"
+    db_session.global_init("db/shop.db")
+    db_sess = db_session.create_session()
+    return render_template('section.html', name=page_name, db_sess=db_sess, sect=Acc)
 
 
 if __name__ == '__main__':
